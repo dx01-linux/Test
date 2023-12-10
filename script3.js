5b01aa851dac7532a5548dfc0c2550211c3b6536
dx01-linux <153017917+dx01-linux@users.noreply.github.com>
2023-12-10 13:27:21 -0600

    12

--- script3.js
+++ script3.js
@@ -1,91 +1,57 @@
-class Attributes{
-        constructor(tag){
-            //objetive tag
-            this.tag = tag;
-            //array of atributes 
-            this.atributes = this.tag.attributes ;
-        }
-        
-        setNewAtribute(attribute , value){
-            this.tag.setAttributes(attribute , value)
-        }
-
-        delAttribute(name){
-            this.tag.attributes.removeNamedItem(name)   
-        }
-}
-
-class Event{
-    constructor(tag , type , instruction){
-            tag.addEventListener(
-                type , 
-                //this.instructions.bind()
-                // arrow f not use this passed as an argument of anonimus func
-                e=>{
-                //instructions
-                }
-            )
+class htmlElement {
+    constructor(tag){
+        this.tag = document.createElement(tag)
+        this.attr = this.tag.attributes ;   
+        this.events = [] ;
     }
-}
-
-class Element {
-    constructor(type){
-        //tag
-        this.tag.bind() = document.createElement(type);
-        //atributes
-        this.tagAttributes = new Attributes(this.tag);
-        //arr of events added
-        this.events = {};
-    }
-
-    addEvent(name , type , instruction){
-        this.events[name] = new Event(this.tag , type , instruction);
-    }
-    delEvent(name){
-        delete this.events[name] ;
-    }
-    cleanEvents(){
-        for(let key of this.events){
-            delete key ;
-        }
-    }
-}
-
-class Wrapper extends Element {
-    constructor(className){
-        super('div');
-        //setting atributes
-        this.setNewAtribute('class' , className);        
-    }
-}
-class Text extends Element {
-    constructor(type , text){
-        super(type);      
-        //setting atributes
-        this.setText(text);
-    }
-
-    setText(text){
-        super.tag.innerHTML = '';
-        this.tag.appendChild(document.createTextNode(text));
-    }
-}
-class Image{
-    constructor(src = '#'){
-        super('img');
-
-        //setting atributes
-        this.tagAttributes.setNewAtribute('src' , src);
+    getTag(){
+        return this.tag ;
     }
     
-}
-
-class Box {
-    constructor(){
-        this.header = new Text('h6' , 'Header');
-        this.image = new Image();
-        this.description = new Text('p' , 'description')
-        this.wrapper = new Wrapper('box')
+    setAttr(type , value){
+        this.tag.setAttribute(type , value)
     }
-
+    delAttr(name) {
+        this.attr.removeNamedItem(name)
+    }
+    appendElement(tag){
+        this.tag.appendElement(tag);
+    }
 }
+
+class Wrapper extends htmlElement{
+    constructor(className){
+        super('div');
+
+        //setting properties
+        this.setAttr('class' , className);
+    }
+}
+
+class Img extends htmlElement{
+    constructor(src = '#' ){
+        super('div');
+
+        //setting properties
+        this.setAttr('src' , src);
+    }
+}
+
+class Text extends htmlElement {
+    constructor(text){
+        super('p');
+
+        //attributes
+        this.appendElement(document.createTextNode(text));
+    }
+}
+
+class Box{
+    constructor(){
+        this.wrapper = new Wrapper('box');
+        this.img = new Img();
+        this.header = new Text('header');
+        this.description = new Text('bla-bla-bla');
+    }
+}
+
